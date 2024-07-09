910a5deba436cf6ee3fed9f1754fa88f334a3a9f
mactavish91 <58135925+mactavish91@users.noreply.github.com>
2024-07-09 16:29:00 +0800

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,7 +3,7 @@
 set -ex
 # pip install video2dataset
 
-#python save_video_txt.py
+python save_video_txt.py
 rm -rf ./videos ./tmp
 mkdir ./tmp
 video2dataset --url_list="videos.txt" --output_folder="videos" --input_format "txt" --output_format "files" --tmp_dir "./tmp"
